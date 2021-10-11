e23e1fe23e6b66883bafba6f646110b55dc14b59
Thomas E <einstein.thomas2@gmail.com>
2021-10-11 09:48:06 -0700

    run init scripts in run function

--- scripts/init_scraped_data.py
+++ scripts/init_scraped_data.py
@@ -2,8 +2,9 @@ from django.conf import settings
 from django.utils.module_loading import import_string
 from time import sleep
 
-for scraper_script_str in settings.SCRAPER['INIT_SCRIPTS']:
-    print('Running script', scraper_script_str)
-    sleep(settings.SCRAPER['PAUSE_SECS'])
-    import_string(f"scripts.{scraper_script_str}.run")()
-    print('\n')
+def run():
+    for scraper_script_str in settings.SCRAPER['INIT_SCRIPTS']:
+        print('Running script', scraper_script_str)
+        sleep(settings.SCRAPER['PAUSE_SECS'])
+        import_string(f"scripts.{scraper_script_str}.run")()
+        print('\n')
