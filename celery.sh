c86ba274843ad8c97af1bee60d57a2f7999600cb
Thomas E <einstein.thomas2@gmail.com>
2021-10-18 20:25:17 -0700

    changed celery dyno

--- celery.sh
+++ celery.sh
@@ -2,4 +2,5 @@
 # This script was made to get free dynos on Heroku.
 celery -A core worker -l info &
 sleep 10
-celery -A core beat -l info &
+echo "Starting Celery beat"
+celery -A core beat -l info
